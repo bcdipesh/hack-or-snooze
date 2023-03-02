83748d323f1983716bc07ebf7b2badca01a686d7
bc-dipesh <bcdipeshwork@gmail.com>
2023-03-02 09:46:21 -0500

    Implement UI for add new story

--- js/stories.js
+++ js/stories.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // This is the global list of the stories, an instance of StoryList
 let storyList;
@@ -6,10 +6,10 @@ let storyList;
 /** Get and show stories when site first loads. */
 
 async function getAndShowStoriesOnStart() {
-  storyList = await StoryList.getStories();
-  $storiesLoadingMsg.remove();
+	storyList = await StoryList.getStories();
+	$storiesLoadingMsg.remove();
 
-  putStoriesOnPage();
+	putStoriesOnPage();
 }
 
 /**
@@ -20,10 +20,10 @@ async function getAndShowStoriesOnStart() {
  */
 
 function generateStoryMarkup(story) {
-  // console.debug("generateStoryMarkup", story);
+	// console.debug("generateStoryMarkup", story);
 
-  const hostName = story.getHostName();
-  return $(`
+	const hostName = story.getHostName();
+	return $(`
       <li id="${story.storyId}">
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
@@ -38,15 +38,42 @@ function generateStoryMarkup(story) {
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
 function putStoriesOnPage() {
-  console.debug("putStoriesOnPage");
+	console.debug('putStoriesOnPage');
 
-  $allStoriesList.empty();
+	$allStoriesList.empty();
 
-  // loop through all of our stories and generate HTML for them
-  for (let story of storyList.stories) {
-    const $story = generateStoryMarkup(story);
-    $allStoriesList.append($story);
-  }
+	// loop through all of our stories and generate HTML for them
+	for (let story of storyList.stories) {
+		const $story = generateStoryMarkup(story);
+		$allStoriesList.append($story);
+	}
 
-  $allStoriesList.show();
+	$allStoriesList.show();
 }
+
+/** Handle create story form submission. If create story is ok, puts new story on page */
+
+async function createNewStory(evt) {
+	console.debug('createNewStory', evt);
+	evt.preventDefault();
+
+	// grab the author, title and url
+	const author = $('#story-author').val();
+	const title = $('#story-title').val();
+	const url = $('#story-url').val();
+
+	const newStory = await StoryList.addStory(currentUser, {
+		author,
+		title,
+		url,
+	});
+
+	$createStoryForm.trigger('reset');
+
+	const $story = generateStoryMarkup(newStory);
+	$allStoriesList.prepend($story);
+
+	$createStoryForm.hide('ease');
+}
+
+$createStoryForm.on('submit', createNewStory);
